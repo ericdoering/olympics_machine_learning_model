1bf6e41977cb305ff018e2bd5d94cc629ebe447c
Eric Doering <ericbenjamindoering@gmail.com>
2025-04-14 14:53:30 -0400

    setup linear regression model

--- run.py
+++ run.py
@@ -2,6 +2,7 @@ import os
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
 
 if os.path.isfile("teams.csv") and os.access("teams.csv", os.R_OK):
     columns_to_keep = ["team", "country", "year", "athletes", "age", "prev_medals", "medals"]
@@ -11,6 +12,20 @@ if os.path.isfile("teams.csv") and os.access("teams.csv", os.R_OK):
     # print(teams.head(10))
     # sns.lmplot(x='athletes', y='medals', data=teams, fit_reg=True, ci=None)
     # sns.lmplot(x='age', y='medals', data=teams, fit_reg=True, ci=None)
+    reg = LinearRegression()
+    
+    train = teams[teams["year"] < 2012].copy()
+    test = teams[teams["year"] >= 2012].copy()
+
+    predictors = ["athletes", "prev_medals"]
+    target = "medals"
+
+    reg.fit(train[predictors], train[target])
+    predictions = reg.predict(test[predictors])
+    test["predictions"] = predictions
+
+    print(predictions[0:20])
+
     # teams.plot.hist(y="medals")
     # plt.show()
 else:
